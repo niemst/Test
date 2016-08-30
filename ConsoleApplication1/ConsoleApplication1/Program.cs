aa23051a0ce3cb9cdfee45ef82880c9b95317568
niemst <niemst@gmail.com>
2016-08-30 11:56:18 +0200

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test2");
+            Console.WriteLine("Test3");
         }
     }
 }
